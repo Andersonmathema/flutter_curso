9979291552a194cdd5b81923b82da35b9ace9264
Andersonmathema <ander.math89@gmail.com>
2026-07-15 23:51:44 -0300

    Fim da aula 43 - Configurando variáveis final e const

--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 
-class StyledText extends StatelessWidget{
-  const StyledText({super.key});
+class StyledText extends StatelessWidget {
+  const StyledText(this.text, {super.key});
+
+  final String text;
 
   @override
   Widget build(context) {
     return Text(
-              'Hello world!',
-              style: TextStyle(
-                color:Colors.white,
-                fontSize: 28,
-              ),
-            );
+      text,
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 28,
+      ),
+    );
   }
 }
